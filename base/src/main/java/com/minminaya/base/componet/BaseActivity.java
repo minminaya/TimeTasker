8c8b7e59a219a8cb6cba6b6f8a6ac2f67648bfbe
minminaya <minminaya@gmail.com>
2018-04-22 19:58:38 +0800

    MODIFY: 修改BaseActivity，自动解绑ButterKnife

--- base/src/main/java/com/minminaya/base/componet/BaseActivity.java
+++ base/src/main/java/com/minminaya/base/componet/BaseActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.trello.navi2.component.support.NaviAppCompatActivity;
 
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * <p>Activity基类的封装，由于考虑到要使用RxJava，这里封装了RxLifecycle</p>
@@ -13,21 +14,24 @@ import butterknife.ButterKnife;
  * <p>Email:minminaya@gmail.com</p>
  */
 public abstract class BaseActivity extends NaviAppCompatActivity {
+    private Unbinder mUnbinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setContentViewLayout_1());
-        ButterKnife.bind(this);
+        mUnbinder = ButterKnife.bind(this);
         initView_2();
-        setListeners_3();
-        bindLogic_4();
+        bindLogic_3();
+        setListeners_4();
         excuteOthers_5();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unBind();
+        unBindLogic();
+        mUnbinder.unbind();
     }
 
     /**
@@ -43,12 +47,12 @@ public abstract class BaseActivity extends NaviAppCompatActivity {
     /**
      * view的监听器绑定
      */
-    protected abstract void setListeners_3();
+    protected abstract void setListeners_4();
 
     /**
      * 绑定
      */
-    protected abstract void bindLogic_4();
+    protected abstract void bindLogic_3();
 
     /**
      * 必要的逻辑
@@ -58,5 +62,5 @@ public abstract class BaseActivity extends NaviAppCompatActivity {
     /**
      * 在onDestroy中的解绑
      */
-    protected abstract void unBind();
+    protected abstract void unBindLogic();
 }
